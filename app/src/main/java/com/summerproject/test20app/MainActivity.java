00a0b1034f0e720802e9c1e1463303286bcd35c2
ryanratliffe <ryanoliverratliffe@gmail.com>
2020-06-19 16:08:02 -0700

    dont fucking break again plz

---
+++ app/src/main/java/com/summerproject/test20app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.summerproject.test20app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/summerproject/test20app/ui/home/history_menu.java
@@ -0,0 +1,27 @@
+package com.summerproject.test20app.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.summerproject.test20app.R;
+
+public class history_menu extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_menu);
+    }
+
+    private void setContentView(int history_menu) {
+    }
+}
