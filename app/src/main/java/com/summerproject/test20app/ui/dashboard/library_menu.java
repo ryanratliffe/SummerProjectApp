29de57c3d47e68fdeb5c4ccfa9df4ff398ccb59c
ryanratliffe <ryanoliverratliffe@gmail.com>
2020-06-23 17:17:04 -0700

    transition between fragments works

--- app/src/main/java/com/summerproject/test20app/ui/dashboard/library_menu.java
+++ app/src/main/java/com/summerproject/test20app/ui/dashboard/library_menu.java
@@ -15,12 +15,16 @@ import com.summerproject.test20app.R;
 public class library_menu extends Fragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.library_menu);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.library_menu, container, false);
     }
 
-    private void setContentView(int library_menu) {
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
     }
-
 }
--- app/src/main/java/com/summerproject/test20app/ui/home/history_menu.java
+++ app/src/main/java/com/summerproject/test20app/ui/home/history_menu.java
@@ -10,13 +10,17 @@ import androidx.fragment.app.Fragment;
 import com.summerproject.test20app.R;
 
 public class history_menu extends Fragment {
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.history_menu);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.history_menu, container, false);
     }
 
-    private void setContentView(int history_menu) {
+    // This event is triggered soon after onCreateView().
+    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Setup any handles to view objects here
+        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
     }
 }
